9218fb034db895add5ee41dd43912e0a6861e9bd
Tobias Munk <schmunk@usrbin.de>
2014-12-16 03:57:24 +0100

    added controller, module and bootstrapping

---
+++ Module.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace schmunk42\markdocs;
+
+/**
+ * This is just an example.
+ */
+class Module extends \yii\base\Module
+{
+
+}
---
+++ controllers/DefaultController.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * @link http://www.diemeisterei.de/
+ * @copyright Copyright (c) 2014 diemeisterei GmbH, Stuttgart
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace schmunk42\markdocs\controllers;
+
+/**
+ * Class DefaultController
+ * @author Tobias Munk <tobias@diemeisterei.de>
+ */
+class DefaultController {
+    /**
+     * Renders the documentation pages from github.com
+     * @return string
+     */
+    public function actionIndex($file = '10-about.md')
+    {
+        // TOOD: DRY(!)
+        $cacheKey = 'github-markdown/toc';
+        $toc      = \Yii::$app->cache->get($cacheKey);
+        if (!$toc) {
+            $toc = $this->createHtml('README.md');
+            \Yii::$app->cache->set($cacheKey, $toc, 300);
+        }
+
+        $cacheKey = 'github-markdown/' . $file;
+        $html     = \Yii::$app->cache->get($cacheKey);
+        if (!$html) {
+            $html = $this->createHtml($file);
+            \Yii::$app->cache->set($cacheKey, $html, 300);
+        }
+
+        $this->layout = 'container';
+        return $this->render(
+            'docs',
+            [
+                'html'     => $html,
+                'toc'      => $toc,
+                'headline' => $file,
+                'forkUrl'  => 'https://github.com/phundament/app/blob/master/docs/' . $file
+            ]
+        );
+    }
+
+    /**
+     * Helper function for the docs action
+     * @return string
+     */
+    private function createHtml($file)
+    {
+        $markdown = file_get_contents('https://raw.githubusercontent.com/phundament/app/master/docs/' . $file);
+        $html     = Markdown::process($markdown, 'gfm');
+        $url      = Url::to(['site/docs', 'file' => '']);
+        $html     = preg_replace('/<a href="(?!http)(.+\.md)">/U', '<a href="__URL__/$1">', $html);
+        $trans    = ['__URL__' => $url];
+        $html     = strtr($html, $trans);
+        return $html;
+    }
+
+}
